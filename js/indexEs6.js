3e562c61d6099a6f3fd11d4cbc46b4fba1a4397f
madiha-eman <madihaema1@gmail.com>
2022-06-24 11:11:50 +0500

    'fixederror'

--- js/indexEs6.js
+++ js/indexEs6.js
@@ -1,5 +1,6 @@
 console.log('EcmaScript version 6')
 let books_records;
+let book;
 class Book{
     constructor(name, author, type){
     this.name = name;
@@ -8,33 +9,34 @@ class Book{
 }
 }
 class Display{
-    add(book){
-            //  books_records = JSON.parse(localStorage.getItem('books'))
-            //  if(books_records){
-                let tableBody = document.getElementById('tableBody');
-                // books_records.forEach(book =>{
+     
+    // add(){
+    //         books_records = JSON.parse(localStorage.getItem('books'))
+    //         if(books_records){
+    //             let tableBody = document.getElementById('tableBody');
+    //             books_records.forEach(function(book){
 
             
-                let uiString = `<tr>
-                                <td>${book.name}</td>
-                                <td>${book.author}</td>
-                                <td>${book.type}</td>
-                            </tr>`;
-            tableBody.innerHTML += uiString
-        // })
+    //             let uiString = `<tr>
+    //                             <td>${book.name}</td>
+    //                             <td>${book.author}</td>
+    //                             <td>${book.type}</td>
+    //                         </tr>`;
+    //         tableBody.innerHTML += uiString
+    //         console.log('book has been added')
+    //     })
 
-             }
+    //          }
                
-        
+    //         }
         
        
-
     clear(){
         let libraryForm = document.getElementById("libraryForm");
             libraryForm.reset();
         }
     validate(book){
-        if(book.name.length < 2 || book.author.length<2){
+        if(book.name.length < 2 && book.author.length<2 || book.name.value === "" && book.author.value === ""){
             return false
         } else{
             return true
@@ -57,16 +59,49 @@ class Display{
                             </div>`;
         setTimeout(function () {
             message.innerHTML = ''
-        }, 5000);
+        }, 2000);
     
     }
 }
+function add(){
+    books_records = JSON.parse(localStorage.getItem('books'))
+    if(books_records){
+        books_records = JSON.parse(localStorage.getItem('books'))
+        let tableBody = document.getElementById('tableBody');
+        books_records.forEach(function(book,id){
+
+    
+        let uiString = `<tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                        <td> <div class="col-2" id=${id} onClick="deleteBook(this.id)">
+                        <button type="submit" class="btn btn-primary">Delete</button>
+                    </div></td>
+                    </tr>
+                  `;
+    tableBody.innerHTML += uiString
+    console.log('book has been added')
+})
+
+     }
+       
+    }
+
+function deleteBook(id){
+        console.log("perfectly it deleted", id)
+        books_records = JSON.parse(localStorage.getItem('books'))
+        books_records.splice(id,1)
+    localStorage.setItem("books", JSON.stringify(books_records))
+//    add()
+    }
 
 let display = new Display();
 // add submit event listner to libraryForm
 let libraryForm = document.getElementById("libraryForm");
 libraryForm.addEventListener('submit', libraryFormSubmit);
 function libraryFormSubmit(e){
+    // e.preventDefault()
 
     // tableBody=localStorage.getItem('tableBody')
     // localStorage.setItem("tableBody")
@@ -87,7 +122,7 @@ function libraryFormSubmit(e){
         type = cooking.value;
     }
     //   book object 
-    let book = new Book(name,author,type)
+    book = new Book(name,author,type)
     books_records = new Array()
     books_records = JSON.parse(localStorage.getItem('books'))? JSON.parse(localStorage.getItem('books')) : [];
    
@@ -99,8 +134,10 @@ function libraryFormSubmit(e){
    'type' : type
 
 })
+        // add();
+
     if(display.validate(book)){
-        display.add(book);
+        // add();
         display.clear();
         display.show('success', 'Your book successfully has been submitted')
         console.log('success')
@@ -109,6 +146,6 @@ function libraryFormSubmit(e){
         display.show('danger', 'Sorry, you cannnot add this book')
     }
     localStorage.setItem('books' ,JSON.stringify(books_records))   
-    e.preventDefault()
     console.log(books_records) 
 }
+add();
